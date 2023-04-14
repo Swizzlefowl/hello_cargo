b5913369f97e3c221744cd4f83875b257552e0de
Arbaz <arbazk1444@gmail.com>
2023-04-14 18:26:08 +0530

    creating a guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
 use std::io;
+use rand::{self, thread_rng, Rng};
+fn main(){
+    let rnum: u32 = thread_rng().gen_range(1..=100);
 
-pub fn fun(x:u32){
-  if x == 10 {
-    println!("your guess was correct");
-}
-else{
- println!("your guess was not correct")
-}
-}
- fn main() {
-   println!("Enter a num:");
-   let mut guess = String::new();
+    loop{
+    let mut num = String::new();
+    println!("eneter a number");
 
-   io::stdin().read_line(&mut guess).expect("failed to readline");
-   let guess:u32 = guess.trim().parse().expect("enter a number");
-  
-  fun(guess);
-}
+    io::stdin().read_line(&mut num).expect("failed to read input");
 
+    let num:u32 = num.trim().parse().expect("failed to get num");
+
+    if num == rnum{
+        println!("good job");
+        break;
+    }
+    else if num > rnum{
+        println!("your guess is bigger then the number")
+    }
+    else if num < rnum{
+        println!("your guess is smaller then the number")
+    }
+    else{
+        println!("you guessed incorrectly\n the num was: {rnum}");
+    }
+}
+}
 
