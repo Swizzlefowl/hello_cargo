103e6fd3bcb00a285fabd11734593cdaebb0e51f
Arbaz <arbazk1444@gmail.com>
2023-04-16 22:36:33 +0530

    use pattern matching instead of if else and other misc imrpovments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-fn main(){
+fn main() {
     loop{
     let mut buffer = String::new();
     let mut num1 = String::new();
@@ -8,6 +8,15 @@ fn main(){
 
     println!("please select an op");
     io::stdin().read_line(&mut buffer).expect("failed to get op");
+    let buffer:char = buffer.trim().parse().expect("failed to parse");
+
+   let op:Operation =  match buffer{
+    '+' => Operation::Addition,
+    '-' => Operation::Subtraction,
+    '*' => Operation::Multiplication,
+    '/' => Operation::Division,
+    _=> continue
+    };
 
     println!("please enter a number");
     io::stdin().read_line(&mut num1).expect("failed to get number");
@@ -17,25 +26,34 @@ fn main(){
     io::stdin().read_line(&mut num2).expect("failed to get message");
     let num2:i32 = num2.trim().parse().expect("failed to parse");
 
-    let op:char = buffer.trim().parse().expect("failed to parse");
+    match  op {
+        Operation::Addition => println!("{}",add(num1, num2)),
+        Operation::Subtraction => println!("{}",sub(num1, num2)),
+        Operation::Multiplication => println!("{}",mul(num1, num2)),
+        Operation::Division => println!("{}",div(num1, num2))
+    };
 
-    if op == '+'{
-        println!("{}", add(num1,num2));
+    println!("do you want to continue?, press 1 to continue and any other key to exit");
+
+    let mut con = String::new();
+    io::stdin().read_line(&mut con).expect("failed to get input");
+
+    let con:i32 = con.trim().parse().expect("failed to parse");
+
+    if con == 1 {
+        continue;
     }
-    else if op == '-'{
-        println!("{}",sub(num1,num2));
-    }
-    else if op == '*'{
-        println!("{}",mul(num1,num2));
-    }
-    else if op == '/'{
-        println!("{}",div(num1,num2));
-    }
-    else{
-        println!("invalid op");
+    else {
         break;
-    }
+    };
 }
+}
+
+pub enum Operation{
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division
 }
 
 fn add(x:i32, y:i32) ->i32{
